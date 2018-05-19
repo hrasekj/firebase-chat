2d83701ca7737deb02c49e28bdd89c1b7418236a
Jakub Hrášek <hrasekj@gmail.com>
2018-05-19 12:28:17 +0200

    Finish firebase chat

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import 'babel-polyfill'
+
 import Firebase from 'firebase'
 import config from '../config/config.local'
 
-Firebase.initializeApp(config.firebase)
+import users from './modules/users'
+import conversations from './modules/conversations'
+
 Vue.use(Vuex)
 
+const fireApp = Firebase.initializeApp(config.firebase)
+const firestore = Firebase.firestore(fireApp)
+
+firestore.settings({ timestampsInSnapshots: true })
+
 const state = {
-    db: Firebase.firestore()
+    db: firestore
 }
 
 export default new Vuex.Store({
   state,
-  modules: {}
+  modules: {
+    users,
+    conversations
+  }
 })
